074eaea0dd608d66693dac9bb26123da4b1e99f3
HIstvan2024 <186374702+HIstvan2024@users.noreply.github.com>
2026-01-16 04:46:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const N8N_WEBHOOK = "https://n8n.srv1254407.hstgr.cloud/webhook/f522d3c0-0cef-45de-bf2b-1abec3d8cb1c";
-const API_KEY = "honlaphonlaphukulcsTeszt26";
+const API_KEY = "honlaphonlaphukulcsTeszt26"; // CSERÉLD KI!
 
 export default {
   async fetch(request) {
